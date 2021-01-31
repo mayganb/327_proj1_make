f64e66438085ca597242f65d981acd98185c10bd
mayganb <maygan.brewer.18@cnu.edu>
2021-01-31 15:54:24 -0500

    commit1

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 //#includes are ALWAYS at top of file
+#include "myfunc.h"
 #include <iostream>	//iostream is where std::cout and std::endl are defined
 
-std::string func()
-{
-	//say hello
-	return "hello";
-}
 
 int main()
 {
